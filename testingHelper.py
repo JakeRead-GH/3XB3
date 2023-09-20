ed0fe90a3df80409d643ddcf3ccb2bf03859bf68
JakeRead-GH <jakeread99@gmail.com>
2023-09-20 17:17:31 -0400

    Added a runs_per_step attribute to the helper to get more consistent times

--- testingHelper.py
+++ testingHelper.py
@@ -5,25 +5,30 @@ import matplotlib.pyplot as plt
 import good_sorts
 
 
-def testSortingAlgorithm(algorithm, highest_list_length, step, rand_list_generator):
+def testSortingAlgorithm(algorithm, highest_list_length, step, runs_per_step, rand_list_generator):
     lengths = []
     times = []
 
     for i in range(0, highest_list_length, step):
-        rand_list = rand_list_generator(i, i)
+        sub_times = []
 
-        start = timeit.default_timer()
-        algorithm(rand_list)
-        end = timeit.default_timer()
+        for j in range(runs_per_step):
+            rand_list = rand_list_generator(i, i)
+
+            start = timeit.default_timer()
+            algorithm(rand_list)
+            end = timeit.default_timer()
+
+            sub_times.append(end - start)
+
+            print(i)
 
         lengths.append(i)
-        times.append(end - start)
-
-        print(i)
+        times.append(sum(sub_times) / runs_per_step)
     return lengths, times
 
 
-def plotTimingGraph(graph_name, sorting_algorithms, highest_list_length, step, rand_list_generator):
+def plotTimingGraph(graph_name, sorting_algorithms, highest_list_length, step, runs_per_step, rand_list_generator):
     plt.title(graph_name)
     plt.xlabel("List Length")
     plt.ylabel("Time (s)")
@@ -31,7 +36,7 @@ def plotTimingGraph(graph_name, sorting_algorithms, highest_list_length, step, r
     algorithm_names = []
 
     for algorithm in sorting_algorithms:
-        lengths, times = testSortingAlgorithm(algorithm, highest_list_length, step, rand_list_generator)
+        lengths, times = testSortingAlgorithm(algorithm, highest_list_length, step, runs_per_step, rand_list_generator)
         plt.plot(lengths, times)
         algorithm_names.append(algorithm.__name__)
 
@@ -39,5 +44,5 @@ def plotTimingGraph(graph_name, sorting_algorithms, highest_list_length, step, r
     plt.show()
 
 
-#plotTimingGraph("Bad Sort Testing", [bad_sorts.bubble_sort, bad_sorts.insertion_sort, bad_sorts.selection_sort], 5000, 50)
-plotTimingGraph("Good Sort Testing", [good_sorts.heapsort, good_sorts.mergesort, good_sorts.quicksort], 5000, 100, bad_sorts.create_random_list)
+#plotTimingGraph("Bad Sort Testing", [bad_sorts.bubble_sort, bad_sorts.insertion_sort, bad_sorts.selection_sort], 5000, 50, 1)
+plotTimingGraph("Good Sort Testing", [good_sorts.heapsort, good_sorts.mergesort, good_sorts.quicksort], 10000, 100, 5, bad_sorts.create_random_list)
