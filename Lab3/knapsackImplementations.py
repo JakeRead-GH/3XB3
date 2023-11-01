19b5943e6bd3f35147e96ed9e308affaadcd4486
JakeRead-GH <jakeread99@gmail.com>
2023-10-31 21:11:17 -0400

    Implemented bottom up and top down dynamic programming solutions for knapsack

--- Lab3/knapsackImplementations.py
+++ Lab3/knapsackImplementations.py
@@ -27,9 +27,60 @@ def ks_rec(items, capacity):
     return ks_rec_core(items, len(items), capacity)
 
 
-items = lab3Helper.generateRandomKnapsackItems(num_items=10)
-#
-# print(ks_brute_force(items, 10))
-print(ks_rec(items, 20))
+def ks_bottom_up(items, capacity):
+    dp = [[-1 for _ in range(capacity + 1)] for _ in range(len(items) + 1)]
 
-lab3Helper.plotTimingGraph("Bad Knapsack Testing", [ks_brute_force, ks_rec], 20, 20)
+    for i in range(len(items) + 1):
+        dp[i][0] = 0
+
+    for j in range(capacity + 1):
+        dp[0][j] = 0
+
+    for i in range(1, len(items) + 1):
+        for j in range(1, capacity + 1):
+            if j - items[i - 1][0] < 0:
+                dp[i][j] = dp[i - 1][j]
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - items[i - 1][0]] + items[i - 1][1])
+
+    return dp[len(items)][capacity]
+
+
+def ks_top_down_core(items, i, j, dp):
+    if (i - 1, j) not in dp:
+        ks_top_down_core(items, i - 1, j, dp)
+
+    if j - items[i - 1][0] < 0:
+        dp[(i, j)] = dp[(i - 1, j)]
+    else:
+        if (i - 1, j - items[i - 1][0]) not in dp:
+            ks_top_down_core(items, i - 1, j - items[i - 1][0], dp)
+
+        dp[(i, j)] = max(dp[(i - 1, j)], dp[(i - 1, j - items[i - 1][0])] + items[i - 1][1])
+
+
+def ks_top_down(items, capacity):
+    if not len(items) or not capacity:
+        return 0
+
+    dp = {}
+
+    for i in range(len(items) + 1):
+        dp[(i, 0)] = 0
+
+    for j in range(capacity + 1):
+        dp[(0, j)] = 0
+
+    ks_top_down_core(items, len(items), capacity, dp)
+
+    return dp[(len(items), capacity)]
+
+
+items = lab3Helper.generateRandomKnapsackItems(num_items=10)
+print(items)
+print(ks_rec(items, 10))
+print(ks_bottom_up(items, 10))
+print(ks_top_down(items, 10))
+
+# lab3Helper.plotTimingGraph("Bad Knapsack Testing", [ks_brute_force, ks_rec], 20, 20)
+lab3Helper.plotTimingGraph("Good Knapsack Testing", [ks_bottom_up, ks_top_down], 100, 100)
