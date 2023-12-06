cd3ce9fe26161f619212403e59902b8ef2be2867
Russell Davidson <russelldavidson10@gmail.com>
2023-12-06 18:19:32 -0500

    Code cleanup

--- Lab4/Part 2&3/experimentSuite2.py
+++ Lab4/Part 2&3/experimentSuite2.py
@@ -40,102 +40,26 @@ def generateLondonSubwayGraph():
             dist = Station.getDistance(station1, station2)
             graph.add_edge(station1.name, station2.name, dist)
             graph.add_edge(station2.name, station1.name, dist)
-            # if station1.name not in heuristics:
-            #     heuristics[station1.name] = {}
-            # if station2.name not in heuristics:
-            #     heuristics[station2.name] = {}
-
-    # Calculate the heuristics for each node
-    # for station1 in stations:
-    #     for station2 in stations:
-    #         dist = Station.getDistance(stations[station1], stations[station2])
-    #         if station1 not in heuristics:
-    #             heuristics[station1] = {}
-    #         heuristics[stations[station1].name][stations[station2].name] = dist
 
     return graph
 
-def generateHeuristicForTarget(graph, target):
+def generateHeuristicForTarget(target):
     h = {}
-    # get target station object
+    # Get target station object
     target_station = None
     for station in stations:
         if stations[station].name == target:
             target_station = stations[station]
+            break
+
+    # Calculate the heuristic for each node
     for station in stations:
-        # Calculate the distance from the station to the target
         h[stations[station].name] = Station.getDistance(stations[station], target_station)
+
     return h
 
 def all_combinations_experiment():
     g = generateLondonSubwayGraph()
-    # Make a 3d graph with x = source, y = destination, z = time
-
-    # x = []
-    # y = []
-    # z = []
-    # for source in g.adj:
-    #     for destination in g.adj:
-    #         if source != destination:
-    #             x.append(source)
-    #             y.append(destination)
-    #             start = timeit.default_timer()
-    #             dijkstra(g, source, destination)
-    #             end = timeit.default_timer()
-    #             z.append(end - start)
-    #             print(source, destination)
-    
-    # fig = plt.figure()
-    # ax = plt.axes(projection='3d')
-    # ax.plot_trisurf(x, y, z, cmap='viridis', edgecolor='none')
-    # ax.set_title('Dijkstra')
-    # ax.set_xlabel('Source')
-    # ax.set_ylabel('Destination')
-    # ax.set_zlabel('Time (s)')
-    # plt.show()
-
-    # plt.title("London Subway: Dijkstra vs A*")
-    # plt.xlabel("Iteration")
-    # plt.ylabel("Time (s)")
-
-    
-    # combinations = []
-    # for source in g.adj:
-    #     for destination in g.adj:
-    #         if source != destination:
-    #             combinations.append((source, destination))
-
-    # # Dijkstra
-    # print("Dijkstra")
-    # time = []
-    # i = 0
-    # for source in g.adj:
-    #     for destination in g.adj:
-    #         if source != destination:
-    #             start = timeit.default_timer()
-    #             dijkstra(g, source, destination)
-    #             end = timeit.default_timer()
-    #             time.append(end - start)
-    #     print(i)
-    #     i+=1
-    # plt.plot(combinations, time)
-    # # A*
-    # print("A*")
-    # time = []
-    # i = 0
-    # for source in g.adj:
-    #     for destination in g.adj:
-    #         if source != destination:
-    #             start = timeit.default_timer()
-    #             a_star(g, source, destination, heuristics[destination])
-    #             end = timeit.default_timer()
-    #             time.append(end - start)
-        
-    #     print(i)
-    #     i+=1
-    # plt.plot(combinations, time)
-    # plt.legend(["Dijkstra", "A*"])
-    # plt.show()
 
 SOURCE = "Greenford"
 DESTINATION = "North Ealing"
